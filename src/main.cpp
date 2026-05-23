e08e46ccebeca1a49a2102747b7cc1d60b900563
Remi Bonnafe <remi.bonnafe@etu.umontpellier.fr>
2026-05-24 01:08:03 +0200

    filet pendu

--- src/main.cpp
+++ src/main.cpp
@@ -65,21 +65,11 @@ int main(void) {
     scene.addMesh(CubeSphereMesh{3});
 
     DynamicObjectDesc filet_desc("filet", 0, DynamicObjectDescPreset::ClothObject);
-    filet_desc.fixed_vertices = {0, size - 1, size * (size - 1), size * size - 1};
+    filet_desc.render_type = DynamicRenderType::LineRender;
+    filet_desc.fixed_vertices = {0};
     Transformation* filet_transfo = scene.addDynamicObject(filet_desc);
     filet_transfo->setScale(glm::vec3(10.f));
-
-    DynamicObjectDesc boule1_desc("boule1", 1, DynamicObjectDescPreset::RigidBody);
-    Transformation* boule1_transformation = scene.addDynamicObject(boule1_desc);
-    boule1_transformation->setTranslation(glm::vec3(3.f, 3.f, 3.f));
-
-    DynamicObjectDesc boule2_desc("boule2", 1, DynamicObjectDescPreset::RigidBody);
-    Transformation* boule2_transformation = scene.addDynamicObject(boule2_desc);
-    boule2_transformation->setTranslation(glm::vec3(0.f, 3.f, 0.f));
-
-    DynamicObjectDesc boule3_desc("boule3", 1, DynamicObjectDescPreset::RigidBody);
-    Transformation* boule3_transformation = scene.addDynamicObject(boule3_desc);
-    boule3_transformation->setTranslation(glm::vec3(-1.f, 1.25f, -1.f));
+    filet_transfo->setTranslation(glm::vec3(5.f, 5.f, 10.f));
 
     scene.resetObjects();
 
